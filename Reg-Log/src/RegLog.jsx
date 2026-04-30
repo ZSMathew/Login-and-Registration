e152322ac2d123899d0b657fead79717b206e88d
ZSMathew <140141453+ZSMathew@users.noreply.github.com>
2026-04-30 14:01:53 +0300

    Update RegLog.jsx

--- Reg-Log/src/RegLog.jsx
+++ Reg-Log/src/RegLog.jsx
@@ -7,8 +7,18 @@ import password_icon from './assets/password.jpg'
 
 const RegLog = () => {
   return (
-    <div>RegLog</div>
-  )
+    <div className='container'>
+        <div className="header">
+            <div className="text">Sign Up</div>
+            <div className="underline"></div>
+        </div>
+        <div className="inputs">
+            <div className="input"> 
+                
+            </div>
+        </div>
+    </div>
+  );
 }
 
 export default RegLog
