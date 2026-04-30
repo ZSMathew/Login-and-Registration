2aff5c65a033debeaf138bcf6c145e0b337d0e22
ZSMathew <140141453+ZSMathew@users.noreply.github.com>
2026-04-30 15:50:11 +0300

    update

--- Reg-Log/src/RegLog.jsx
+++ Reg-Log/src/RegLog.jsx
@@ -14,8 +14,22 @@ const RegLog = () => {
         </div>
         <div className="inputs">
             <div className="input"> 
-                
+             <img src={user_icon} alt='' /> 
+             <input type='text' placeholder='Username'/> 
             </div>
+            <div className="input"> 
+             <img src={email_icon} alt='' /> 
+             <input type='email' placeholder='Email'/> 
+            </div>
+            <div className="input"> 
+             <img src={password_icon} alt='' /> 
+             <input type='password' placeholder='Password'/> 
+            </div>
+        </div>
+        <div className="forgot-password">Lost Password! <span>Click Here!</span></div>
+        <div className="submit-container">
+          <div className="submit">Sign Up</div>
+          <div className="submit">Login</div>
         </div>
     </div>
   );
