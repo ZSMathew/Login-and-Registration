70907eb4fad0897fd4fe3525759d5ec505b70fe8
ZSMathew <140141453+ZSMathew@users.noreply.github.com>
2026-04-30 13:50:15 +0300

    update

--- README.md
+++ README.md
@@ -5,9 +5,10 @@ Creating Registration and Login form Using React
 Creating Project with react
 npm create vite@latest my-react-app
 
-geting into project folder
+geting into project folder 
 cd my-react-app
 npm install
 
 Starting Development Server
 npm run dev
+
---
+++ Reg-Log/src/RegLog.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './RegLog.css' 
+import user_icon from './assets/person.jpg'
+import email_icon from './assets/email.jpg'
+import password_icon from './assets/password.jpg'
+
+
+const RegLog = () => {
+  return (
+    <div>RegLog</div>
+  )
+}
+
+export default RegLog
